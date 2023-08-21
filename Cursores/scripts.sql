567e9308d41bf39d9e7a0740c7ba3d633d26e8b4
erincon01 <7208487+erincon01@users.noreply.github.com>
2023-08-21 18:33:58 +0200

    .

--- Cursores/scripts.sql
+++ Cursores/scripts.sql
@@ -1,26 +1,24 @@
 
--- cursores
 
-select c.session_id, count(*) -- c.cursor_id
-from sys.dm_exec_cursors(0) c
-group by c.session_id
-order by 2 desc
-
-
-select c.session_id, t.text, count(*) cantidad ---- c.cursor_id
+-- cursores por query
+-- sesion, query, cantidad
+select c.session_id, t.text, count(*) cantidad
 from sys.dm_exec_cursors(0) c
 cross apply sys.dm_exec_sql_text (c.sql_handle) t
 group by c.session_id, t.text
-order by 3 desc
+order by cantidad desc
+go
 
-
-select * --top 10 c.session_id, c.cursor_id, c.name, t.*
+-- propiedades cursores 
+-- sesiones, propiedades del cursor, cantidad
+select c.session_id, c.properties, count(*) cantidad
 from sys.dm_exec_cursors(0) c
-cross apply sys.dm_exec_sql_text (c.sql_handle) t
+group by c.session_id, c.properties
+order by cantidad desc
+go
 
-
-select * from sys.dm_exec_connections
-
+-- plan cache
+-- top 20 order by cpu time
 
 SELECT TOP 20 
     SUBSTRING(qt.TEXT, (qs.statement_start_offset/2)+1,
@@ -42,15 +40,16 @@ CROSS APPLY
     sys.dm_exec_sql_text(qs.sql_handle) qt
 CROSS APPLY 
     sys.dm_exec_query_plan(qs.plan_handle) qp
--- ORDER BY 
---    qs.total_logical_reads DESC -- logical reads
--- ORDER BY 
---     qs.total_logical_writes DESC -- logical writes
 ORDER BY 
     qs.total_worker_time DESC -- CPU time
+go
 	
-	
-	
+
+-- conexiones abiertas
+select * from sys.dm_exec_connections
+
+
+-- distribución de memoria 
 
 select type, name, cast( pages_kb * 1.0 / (1024*1024) as decimal(10,2)) gb
 from sys.dm_os_memory_clerks
